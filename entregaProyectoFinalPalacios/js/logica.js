6c00abc871cd7542faf931d535427e1995451f50
Lucas Palacios <86528919+lucasfpalacios@users.noreply.github.com>
2021-12-14 15:58:27 -0300

    Add files via upload

--- entregaProyectoFinalPalacios/js/logica.js
+++ entregaProyectoFinalPalacios/js/logica.js
@@ -1,4 +1,58 @@
+//Array de juegos
+const listaDeJuegos = [{id:"1", nombre:"Assassin Creed Valhalla", categoria:"accion/aventura", precio:30},
+{id:"2", nombre:"The last of us part 2", categoria:"accion/aventura", precio:50},
+{id:"3", nombre:"Cyberpunk 2077", categoria:"accion", precio:60},
+{id:"4", nombre:"Death Stranding", categoria:"aventura", precio:45},
+{id:"5", nombre: "Village", categoria: "accion/aventura", precio:60},
+{id:"6", nombre:"Ghost of Tsushima", categoria:"aventura/accion", precio:60},
+{id:"7", nombre:"Biomutant", categoria:"aventura", precio:50},
+{id:"8", nombre:"Demon's Souls", categoria:"aventura/accion", precio:50},
+{id:"9", nombre:"Spiderman Miles Morales", categoria:"aventura/accion", precio:50},
+{id:"10", nombre:"Returnal", categoria:"aventura", precio:50},
+{id:"11", nombre:"Watch Dogs Legion", categoria:"accion", precio:50},
+{id:"12", nombre:"God of War 4", categoria:"aventura/accion", precio:50},
+{id:"13", nombre:"Crash Bandicoot It's About Time", categoria:"aventura", precio:40},
+{id:"14", nombre:"Worl of Warcraft", categoria:"rol", precio:50},
+{id:"15", nombre:"Valorant", categoria:"shooter", precio:50},
+{id:"16", nombre:"League Of Legends", categoria:"rol", precio:50},
+{id:"17", nombre:"Counter-Strike: Global Offensive", categoria:"shooter", precio:10},
+{id:"18", nombre:"Fortnite", categoria:"shooter", precio:50},
+];
 
+const juegosPrecios = () => {
+  return listaDeJuegos.filter((listaDeJuegos) =>{
+    if(listaDeJuegos.precio <= 50) return true
+    return false
+  })
+}
+console.log(juegosPrecios())
+
+//Formulario de validacion en seccion Index
+
+function captura(){
+  var usuario =  document.getElementById("name").value;
+  console.log(usuario)
+  var pass = document.getElementById("password").value;
+  console.log(pass)
+  if(usuario == ""){
+    alert("Este campo es obligarotrio");
+    document.getElementById("name").focus();
+  }else{
+    if(pass == ""){
+      alert("Este campo es obligarotrio");
+      document.getElementById("password").focus();
+    }else{
+      console.log()
+      document.getElementById("name").value="";
+      document.getElementById("password").value="";
+      document.getElementById("name").focus();
+    }
+  }
+}  
+
+
+
+//Modificacion del DOM
 
 const encabezado__titulo = document.querySelector(".encabezado__titulo")
 
@@ -8,139 +62,157 @@ encabezado__titulo.style.color = "wheat"
 
 //JS PARA MOSTRAR LOS TRAILERS EN LA SECCION DE JUEGOS
 
-$("#valhallaYouTube").click(()=>{
-  $("#acVideo").show()
-})
-$("#acVideo").css({ "width" : "250px" });
+$("#valhallaYouTube").on( "click", function() { 
+  $('#acVideo').show();
+ });
+  $( ".cerrar" ).click(function() {
+  $('#acVideo').hide();
+});
 
 $("#tlouYouTube").click(()=>{
   $("#tlVideo").show()
 })
-$("#tlVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#tlVideo').hide();
+});
+
 
 $("#cyberpunkYouTube").click(()=>{
   $("#cVideo").show()
 })
-$("#cVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#cVideo').hide();
+});
+
 
 $("#deathStrandingYouTube").click(()=>{
   $("#dsVideo").show()
 })
-$("#dsVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#dsVideo').hide();
+});
+
 
 $("#villageYouTube").click(()=>{
   $("#vVideo").show()
 })
-$("#vVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#vVideo').hide();
+});
+
 
 $("#ghostYouTube").click(()=>{
   $("#gVideo").show()
 })
-$("#gVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#gVideo').hide();
+});
+
 
 $("#biomutantYouTube").click(()=>{
   $("#bVideo").show()
 })
-$("#bVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#bVideo').hide();
+});
+
 
 $("#demonYouTube").click(()=>{
   $("#dVideo").show()
 })
-$("#dVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#dVideo').hide();
+});
+
 
 $("#spidermanYouTube").click(()=>{
   $("#mmVideo").show()
 })
-$("#mmVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#mmVideo').hide();
+});
+
 
 $("#returnalYouTube").click(()=>{
   $("#rVideo").show()
 })
-$("#rVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#rVideo').hide();
+});
+
 
 $("#watchDogsYouTube").click(()=>{
   $("#wVideo").show()
 })
-$("#wVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#wVideo').hide();
+});
+
 
 $("#gowYouTube").click(()=>{
   $("#gowVideo").show()
 })
-$("#gowVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#gowVideo').hide();
+});
+
 
 $("#crashYouTube").click(()=>{
   $("#cbVideo").show()
 })
-$("#cbVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#cbVideo').hide();
+});
+
 
 $("#wowYouTube").click(()=>{
   $("#wowVideo").show()
 })
-$("#wowVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#wowVideo').hide();
+});
+
 
 $("#valorantYouTube").click(()=>{
   $("#valVideo").show()
 })
-$("#valVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#valVideo').hide();
+});
+
 
 $("#lolYouTube").click(()=>{
   $("#lolVideo").show()
 })
-$("#lolVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#lolVideo').hide();
+});
+
 
 $("#csgoYouTube").click(()=>{
   $("#csVideo").show()
 })
-$("#csVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#csVideo').hide();
+});
+
 
 $("#fortniteYouTube").click(()=>{
   $("#fVideo").show()
 })
-$("#fVideo").css({ "width" : "250px" });
+$( ".cerrar" ).click(function() {
+  $('#fVideo').hide();
+});
 
 
-
-
-
-
-
-
-// Ricardo intente hacer que se salga el video con un boton pero no pude, una de las maneras que probe fue esta siguiente. Deje un ejemlo del boton a apretar pero tambien queria saber si se podia ocultar presionando en cualquier parte de la pantalla. Si podes aclarame como seria. Muchas Gracias
-
-/*
-let valhalla = $("#valhallaYouTube")
-let tlou = $("#tlouYouTube")
-let cyberpunk = $("#cyberpunkYouTube")
-let deathStranding = $("#deathStrandingYouTube")
-let village = $("#villageYouTube")
-let ghost = $("#ghostYouTube")
-let biomutant = $("#biomutantYouTube")
-let demon = $("#demonYouTube")
-let spidermanM = $("#spidermanYouTube")
-let returnal = $("#returnalYouTube")
-let watchDogs = $("#watchDogsYouTube")
-let godOfWar = $("#gowYouTube")
-let crash = $("#crashYouTube")
-let wow = $("#wowYouTube")
-let valorant = $("#valorantYouTube")
-let lol = $("#lolYouTube")
-let csgo = $("#csgoYouTube")
-let fortnite = $("#fortniteYouTube")
-
-let acVideo = $("#acVideo")
-let salirVideo = $("#ocultarVideo")
-/*
-valhalla.click(()=>{
-  acVideo.show()
+$(".btn").click(()=>{
+  $(".cerrar").show()
 })
-salirVideo.cilck(()=>{
-  acVideo.hide()
-})*/
+$( ".cerrar" ).click(function() {
+  $('.cerrar').hide();
+});
 
-
-
-
-
+$(".cerrar").css({ "display" : "none" , "border-radius" : "4px" , "color" : "white" , "background-color" : "rgb(228, 105, 105)" , "border-color" : "rgb(228, 105, 105)" , "width" : "30px"});
 
 
 
